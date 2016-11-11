79b6612a491f6f55a87ad7922bde361f61334f8a
Antti Reijonen <areijone@gmail.com>
2016-11-11 14:13:43 +0200

    Bootstrap ja pieni demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Grid, Row, Col, Jumbotron} from 'react-bootstrap';
+import Component1 from './Component1.js'
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    /* 
+      Stateen haetaan data factorystä ja
+      pääkomponentti valuttaa sitä alaspäin.
+      
+      Nyt data on määritelty suoraan stateen.
+      (defaultMessage, dataToComponent)
+    */
+    this.state = {
+      defaultMessage: "This is default data coming from state",
+      dataToComponent: [
+        {eka: "Juuh",
+        toka: "elikkäs"}
+      ]
+    } 
+  }
   render() {
+     /*
+            // JSX-tyyli: komponentille annetaan parametrina dataa ylhäältä
+            // ja tässä tapauksessa funktio renderöi näkyviin jotain datan
+            // perusteella. <Component1 ../> yms. Itse komponentti löytyy
+            // tiedostosta Component1.js
+            //
+            // Jumbotronistakin voisi tehdä oman tiedostonsa, jos haluaisi.
+            */
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Row>
+          <Jumbotron>
+            <h1>{this.state.defaultMessage}</h1>
+          </Jumbotron>
+        </Row>
+        <Row>
+          <Col md={6}>
+           
+            <Component1 data={this.state.dataToComponent} />
+          </Col>
+          <Col md={6}>
+            <p>Juuh elikkäs</p>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
