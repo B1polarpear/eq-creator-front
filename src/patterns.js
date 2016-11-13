7f7fa9a756043be618508b4e786a1e11bd738c3f
Toni Ruth <Toni Ruth>
2016-11-13 19:47:55 +0200

    ac ja costit

--- src/patterns.js
+++ src/patterns.js
@@ -4,28 +4,56 @@ const PATTERNS = [
       "name": "Silk",
       "ac": "6",
       "costs": {
-        "Avenger": "10",
-        "Paladin": "20",
-        "Ranger": "30"
-      }
+        "Avenger": "7",
+        "Paladin": "5",
+        "Ranger": "7",
+		"Bard": "19",
+		"Rogue": "24",
+		"Assassin": "32",
+		"Healer": "5",
+		"Druid": "7",
+		"Inquisitor": "7",
+		"Enchanter": "19",
+		"Conjurer": "24",
+		"Necromancer": "32"
+		}
     },
     {
       "id": 1,
       "name": "Suede",
-      "ac": "3",
+      "ac": "12",
       "costs": {
-        "Avenger": "20",
-        "Paladin": "2"
+        "Avenger": "44",
+        "Paladin": "32",
+		"Ranger": "44",
+		"Bard": "101",
+		"Rogue": "131",
+		"Assassin": "176",
+		"Healer": "32",
+		"Druid": "44",
+		"Inquisitor": "44",
+		"Enchanter": "101",
+		"Conjurer": "131",
+		"Necromancer": "176"
       }
     },
     {
       "id": 2,
       "name": "Leather",
-      "ac": "3",
+      "ac": "18",
       "costs": {
-        "Avenger": "20",
-        "Paladin": "2",
-        "Ranger": "30"
+        "Avenger": "120",
+        "Paladin": "86",
+        "Ranger": "120",
+		"Bard": "280",
+		"Rogue": "369",
+		"Assassin": "502",
+		"Healer": "86",
+		"Druid": "120",
+		"Inquisitor": "120",
+		"Enchanter": "280",
+		"Conjurer": "369",
+		"Necromancer": "502"
       }
     }
 ];
